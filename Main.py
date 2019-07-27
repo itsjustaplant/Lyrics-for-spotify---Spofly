d044624de0f38e5c86aa665f596a036ebe0406a4
plænt <alperensahin441@gmail.com>
2019-07-27 23:49:11 +0300

    yay

--- Main.py
+++ Main.py
@@ -1,11 +1,8 @@
-
 from flask import Flask, render_template, redirect, request
 import requests
 import json
 import base64
 import lyricsgenius
-
-
 
 app = Flask(__name__)
 TOKEN = " "
@@ -13,14 +10,16 @@ CLIENT_ID = "e6cd36c6ea1c44b09245097b9e3367e1"
 CLIENT_SECRET = "94c8b9efa0f34440b7c3225b75ff0b37"
 GENIUS_URL = "http://www.genius.com/"
 genius = lyricsgenius.Genius("YXICHA95DGXKPPPkXp-iSddKqjf93dOfxM30rG2s168h6t721l6WGcDt8KpGVO7G")
+
+
+
 @app.route("/")
 def login():
     return render_template(("login.html"))
 @app.route("/login")
 def log():
     return redirect(
-                    "https://accounts.spotify.com/authorize?client_id=e6cd36c6ea1c44b09245097b9e3367e1&response_type=code&redirect_uri=https://spoflyv1.herokuapp.com/callback&scope=user-read-currently-playing")
-
+        "https://accounts.spotify.com/authorize?client_id="+CLIENT_ID+"&response_type=code&redirect_uri=http://localhost:5000/callback&scope=user-read-private user-read-email user-read-currently-playing user-modify-playback-state")
 
 @app.route("/callback")
 def token():
@@ -28,7 +27,7 @@ def token():
     code_payload = {
         "grant_type": "authorization_code",
         "code": str(auth_token),
-        "redirect_uri": "https://spoflyv1.herokuapp.com/callback",
+        "redirect_uri": "http://localhost:5000/callback",
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
     }
@@ -58,32 +57,31 @@ def init():
     song_title = current_song['item']['name']
     artist_name = current_song['item']['artists'][0]['name']
     if current_song['item']['is_local'] == bool(0):
-        image_url = current_song['item']['album']['images'][0]['url']
+     image_url = current_song['item']['album']['images'][0]['url']
     else:
         image_url=""
     duration_ms = current_song['item']['duration_ms']
     progress_ms = current_song['progress_ms']
     refresh_ms = (duration_ms - progress_ms) / 1000 - 15
-    
+
     if refresh_ms < 0:
-        refresh_ms *= -1
+            refresh_ms *= -1
 
     if song_title and artist_name:
-        song = genius.search_song(title=song_title, artist=artist_name)
+     song = genius.search_song(title=song_title, artist=artist_name)
 
-        if song!=None:
-lyrics = song.lyrics
-    
-    else:
+     if song!=None:
+        lyrics = song.lyrics
+
+     else:
         lyrics = "lyrics not found"
 
-else:
-    lyrics = "if you are playing a local file please edit metadata"
-            
-            
+    else:
+        lyrics = "if you are playing a local file please edit metadata"
 
     return render_template("home.html", data=lyrics, artist_name=artist_name, song_title=song_title,
-                       image=image_url, refresh_ms=refresh_ms)
+                           image=image_url, refresh_ms=refresh_ms)\
+
 @app.route("/next")
 def next():
     headers = {
