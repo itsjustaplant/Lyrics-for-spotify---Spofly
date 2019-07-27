d3f606b7126e2f3d5d6a40305034d52d7fdeb4d4
plænt <alperensahin441@gmail.com>
2019-07-27 23:52:00 +0300

    yay

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ def login():
 @app.route("/login")
 def log():
     return redirect(
-        "https://accounts.spotify.com/authorize?client_id="+CLIENT_ID+"&response_type=code&redirect_uri=http://localhost:5000/callback&scope=user-read-private user-read-email user-read-currently-playing user-modify-playback-state")
+         "https://accounts.spotify.com/authorize?client_id=e6cd36c6ea1c44b09245097b9e3367e1&response_type=code&redirect_uri=https://spoflyv1.herokuapp.com/callback&scope=user-read-currently-playing")
 
 @app.route("/callback")
 def token():
@@ -27,7 +27,7 @@ def token():
     code_payload = {
         "grant_type": "authorization_code",
         "code": str(auth_token),
-        "redirect_uri": "http://localhost:5000/callback",
+        "redirect_uri": "https://spoflyv1.herokuapp.com/callback",
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
     }
